b7321d0ecec49674249e6f923915318f519262c6
Santiago <santiperez1199@gmail.com>
2021-03-28 18:49:24 +0200

    change color radio button

--- src/components/atoms/RadioButton.js
+++ src/components/atoms/RadioButton.js
@@ -1,27 +1,18 @@
 // Importing react utilities
-import React, { useContext, useState } from 'react';
-import { StyleSheet, View, ImageBackground, SafeAreaView, Text, TextInput } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { RadioButton } from 'react-native-paper';
-import FormInput from '../../components/atoms/FormInput';
+import React from 'react';
 
-// Importing icons
-import Icon from 'react-native-vector-icons/AntDesign';
+import { RadioButton } from 'react-native-paper';
 
 // Importing components
 import * as Colors from '../../styles/colors';
-import Button from '../../components/atoms/Button';
-import Loading from '../../components/atoms/Loading';
-import { AuthContext } from '../../navigation/AuthProvider';
+
 
 export default function RadioButtonComponent({ ...props }) {
-    const [value, setValue] = React.useState('');
-
     return (
         <RadioButton.Group onValueChange={props.onValueChange} value={props.value}>
-            <RadioButton.Item label="Female" value="Female" />
-            <RadioButton.Item label="Male" value="Male" />
-            <RadioButton.Item label="Specify another" value="Other" />
+            <RadioButton.Item label="Female" value="Female" color={Colors.PRIMARY}/>
+            <RadioButton.Item label="Male" value="Male" color={Colors.PRIMARY}/>
+            <RadioButton.Item label="Specify another" value="Other" color={Colors.PRIMARY}/>
         </RadioButton.Group>
     );
 }

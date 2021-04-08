dfb06d26065cd80e4e3378d0eed8388b400f8df3
Santiago <santiperez1199@gmail.com>
2021-04-08 20:42:31 +0200

    Getting and updating user data implemented

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -1,112 +1,281 @@
 // Importing react utilities
 import React, { useContext, useState, useEffect } from 'react';
 
-import { StyleSheet, View, SafeAreaView, Text, TextInput, ScrollView } from 'react-native';
-import { Card, Avatar } from 'react-native-elements';
+import { StyleSheet, View, SafeAreaView, Text, TextInput, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
+import { Card } from 'react-native-elements';
 
 // Importing components
 import * as Colors from '../styles/colors';
 import Button from '../components/atoms/Button'
 import Selector from '../components/molecules/Selector'
-import { firebase } from '../firebase/index'
+import { editUser } from '../firebase/Logic'
+import ProfileAvatar from '../components/atoms/ProfileAvatar';
+import { AuthContext } from '../navigation/AuthProvider';
+
+// Importing icons
+import Icon from 'react-native-vector-icons/Ionicons';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
+
+// Importing image paths
+import { images } from '../utils/images'
 
 // Lists
 import listOfHobbies from '../utils/hobbies'
 import listOfLanguages from '../utils/languages'
 import listOfCountries from '../utils/countries'
 
+export default function EditProfileScreen({ navigation, route }) {
 
-export default function EditProfileScreen({ navigation }) {
-  const [description, onChangeText] = React.useState('The idea with React Native Elements is more about component structure than actual design.The idea with React Native Elements is more about');
+  var user = route.params;
 
-  const [selectedHobbies, onChangeHobbies] = useState([])
-  const [selectedLanguages, onChangeLanguages] = useState([])
-  const [selectedCountries, onChangeCountries] = useState([])
-  const [user, setUser] = React.useState([])
+  const [name, setName] = useState(user.name)
+  const [currentLocation, setCurrentLocation] = useState(user.current_location)
+  const [profilePicture, setProfilePicture] = useState(user.profile_picture);
+  const [description, setDescription] = useState(user.about_me);
+  const [userType, setUserType] = useState(user.user_type);
 
+  const [selectedHobbies, onChangeHobbies] = useState(user.hobbies)
+  const [selectedLanguages, onChangeLanguages] = useState(user.languages)
+  const [selectedCountries, onChangeCountries] = useState(user.countries)
 
-  useEffect(() => {
+  const { loading, setLoading } = useContext(AuthContext);
 
-    var userId = firebase.auth().currentUser.uid;
-    console.log(userId)
+  const updateUserData = async () => {
 
-    const subscriber = firebase.firestore()
-      .collection('users')
-      .doc(userId)
-      .onSnapshot(documentSnapshot => {
-        setUser(documentSnapshot.data())
-        console.log(user)
-      });
+    setLoading(true);
 
-    // Stop listening for updates when no longer required
-    return () => subscriber();
-  },[]);
+    var userData = {
+            name: name,
+            profile_picture: profilePicture,
+            current_location: currentLocation,
+            user_type: userType,
+            about_me: description,
+            countries: selectedCountries,
+            languages: selectedLanguages,
+            hobbies: selectedHobbies,
+    }
+
+    await editUser(userData)
+
+    setLoading(false);
+
+    navigation.goBack()
+
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Avatar
-          size="xlarge"
-          width={styles.profile_picture.width}
-          height={styles.profile_picture.height}
-          rounded
-          placeholder="hola"
-          source={require("../assets/images/profile_picture.jpg")}
-          imageProps={{ resizeMode: 'cover' }} // Rescaling the image
-        />
-      </View>
-      <View style={styles.card_container}>
-        <Card containerStyle={styles.card}>
-          <ScrollView contentContainerStyle={styles.scrollview}>
-            <View style={styles.description_container}>
-              <Text style={styles.description_title}>
-                About me
-            </Text>
-              <TextInput
-                style={styles.text_input}
-                multiline
-                editable
-                maxLength={200}
-                onChangeText={description => onChangeText(description)}
-                value={description}
-                blurOnSubmit={true}
-              />
-            </View>
-            <View style={styles.selector_container}>
-              <Selector listName="hobbies" list={listOfHobbies} onSelectedItemObjectsChange={(selectedItems) => onChangeHobbies(selectedItems)}></Selector>
-            </View>
-            <View style={styles.selector_container}>
-              <Selector listName="languages" list={listOfLanguages} onSelectedItemObjectsChange={(selectedItems) => onChangeLanguages(selectedItems)}></Selector>
-            </View>
-            <View style={styles.selector_container}>
-              <Selector listName="countries" list={listOfCountries} onSelectedItemObjectsChange={(selectedItems) => onChangeCountries(selectedItems)}></Selector>
-            </View>
-            <View style={styles.buttons_container}>
-              <Button style={styles.save_button} title="Save"
-                onPress={() => { }}></Button>
-              <Button style={styles.cancel_button} title="Cancel"
-                onPress={() => navigation.goBack()}></Button>
-            </View>
-            <View style={styles.scrollview_bottom}></View>
-          </ScrollView>
-        </Card>
-      </View>
+      <ImageBackground source={images.signUpBackground.uri} style={styles.background}>
+        <View style={styles.header}>
+          <ProfileAvatar
+            size="medium"
+            width={styles.profile_picture.width}
+            height={styles.profile_picture.height}
+            selectedImage={profilePicture}
+            onImageChange={(img) => {
+              setProfilePicture(img)
+            }}
+            iconStyle={styles.edit_icon}
+            containerIconStyle={styles.edit_picture}
+          ></ProfileAvatar>
+          <View>
+            <TextInput
+              value={name}
+              style={styles.text_input_name}
+              onChangeText={(txt) => setName(txt)}
+              autoCapitalize="words"
+              inlineImageLeft='search_icon'
+            ></TextInput>
+            <Text style={styles.city}>{user.current_location}</Text>
+          </View>
+        </View>
+        <View style={styles.local_or_tripver}>
+          <TouchableOpacity
+            onPress={() => setUserType("local")}
+            style={userType == "local" ? styles.local_or_tripver_container_selected : styles.local_or_tripver_container}>
+            <Text style={userType == "local" ? styles.local_or_tripver_text_selected : styles.local_or_tripver_text}>Local</Text>
+            <Icon2
+              name='city-variant-outline'
+              color={userType == "local" ? Colors.WHITE : Colors.SECONDARY}
+              size={25}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setUserType("tripver")}
+            style={userType == "tripver" ? styles.local_or_tripver_container_selected : styles.local_or_tripver_container}>
+            <Text style={userType == "tripver" ? styles.local_or_tripver_text_selected : styles.local_or_tripver_text}>Tripver</Text>
+            <Icon2
+              name='bag-personal-outline'
+              color={userType == "tripver" ? Colors.WHITE : Colors.SECONDARY}
+              size={28}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card_container}>
+          <Card containerStyle={styles.card}>
+            <ScrollView
+              contentContainerStyle={styles.scrollview}
+              showsVerticalScrollIndicator={false}>
+              <View style={styles.description_container}>
+                <Text style={styles.description_title}>
+                  Name
+                </Text>
+                <TextInput
+                  style={styles.text_input}
+                  multiline
+                  editable
+                  maxLength={20}
+                  onChangeText={txt => setName(txt)}
+                  value={name}
+                  blurOnSubmit={true}
+                />
+                <Icon
+                  name='ios-person'
+                  color={Colors.GRAY_MEDIUM}
+                  size={20}
+                  style={styles.name_icon}
+                />
+              </View>
+              <TouchableOpacity style={styles.description_container} onPress={() => console.log("dfsg")}>
+                <Text style={styles.description_title}>
+                  Location
+                </Text>
+                <Text style={styles.text_input}>{currentLocation}</Text>
+                <Icon
+                  name='location-sharp'
+                  color={Colors.GRAY_MEDIUM}
+                  size={25}
+                  style={styles.location_icon}
+                />
+              </TouchableOpacity>
+              <View style={styles.description_container}>
+                <Text style={styles.description_title}>
+                  About me
+                </Text>
+                <TextInput
+                  style={styles.text_input}
+                  multiline
+                  editable
+                  maxLength={200}
+                  onChangeText={description => setDescription(description)}
+                  value={description}
+                  blurOnSubmit={true}
+                />
+              </View>
+              <View style={styles.selector_container}>
+                <Selector listName="hobbies" list={listOfHobbies} selectedItems={selectedHobbies} onSelectedItemObjectsChange={(selectedItems) => onChangeHobbies(selectedItems)}></Selector>
+              </View>
+              <View style={styles.selector_container}>
+                <Selector listName="languages" list={listOfLanguages} selectedItems={selectedLanguages} onSelectedItemObjectsChange={(selectedItems) => onChangeLanguages(selectedItems)}></Selector>
+              </View>
+              <View style={styles.selector_container}>
+                <Selector listName="countries" list={listOfCountries} selectedItems={selectedCountries} onSelectedItemObjectsChange={(selectedItems) => onChangeCountries(selectedItems)}></Selector>
+              </View>
+              <View style={styles.buttons_container}>
+                <Button
+                  style={styles.save_button}
+                  title="Save"
+                  onPress={() => { updateUserData() }}></Button>
+                <Button
+                  style={styles.cancel_button}
+                  title="Cancel"
+                  onPress={() => navigation.goBack()}></Button>
+              </View>
+              <View style={styles.scrollview_bottom}></View>
+            </ScrollView>
+          </Card>
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+  },
   container: {
     backgroundColor: Colors.PRIMARY,
     flex: 1,
   },
   header: {
+    flexDirection: 'row',
     alignItems: 'center',
+    alignContent: 'space-around',
     margin: 20,
+    marginLeft: 20,
+    marginTop: 40,
   },
   profile_picture: {
     width: 90,
     height: 90
+  },
+  edit_picture: {
+    height: '25%',
+    width: '25%',
+    borderRadius: 30,
+    backgroundColor: Colors.SECONDARY,
+  },
+  edit_icon: {
+    fontSize: 15,
+  },
+  text_input_name: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  city: {
+    color: 'white',
+    fontSize: 15,
+    marginLeft: 20,
+  },
+
+  local_or_tripver: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    margin: 5,
+  },
+
+  local_or_tripver_container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: Colors.WHITE,
+    backgroundColor: Colors.WHITE,
+    width: '40%',
+    height: '100%',
+    padding: 5,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: Colors.WHITE
+  },
+
+  local_or_tripver_container_selected: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: Colors.SECONDARY,
+    width: '40%',
+    height: '100%',
+    padding: 5,
+    borderRadius: 10,
+    borderColor: Colors.WHITE,
+    borderWidth: 2,
+  },
+
+  local_or_tripver_text: {
+    color: Colors.PRIMARY,
+    fontWeight: 'bold',
+  },
+
+  local_or_tripver_text_selected: {
+    color: Colors.WHITE,
+    fontWeight: 'bold',
   },
 
   card_container: {
@@ -121,6 +290,7 @@ const styles = StyleSheet.create({
     height: '100%',
     alignSelf: 'center',
     padding: 0,
+    marginTop: 20,
   },
 
   scrollview: {
@@ -129,7 +299,7 @@ const styles = StyleSheet.create({
   },
 
   scrollview_bottom: {
-    height: 120,
+    height: 250,
   },
 
   description_container: {
@@ -144,13 +314,27 @@ const styles = StyleSheet.create({
 
   description_title: {
     fontWeight: 'normal',
-    fontSize: 16,
+    fontSize: 15,
     marginLeft: 5,
   },
 
   text_input: {
     padding: 5,
     color: Colors.PRIMARY,
+  },
+
+  name_icon: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    top: '40%',
+    right: '3%'
+  },
+
+  location_icon: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    top: '36%',
+    right: '2%'
   },
 
   selector_container: {

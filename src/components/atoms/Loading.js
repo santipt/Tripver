2022be67e9983c2661f7277fabddd9552364802b
Santiago <santiperez1199@gmail.com>
2021-04-12 22:23:15 +0200

    Customised loading animation implemented

--- src/components/atoms/Loading.js
+++ src/components/atoms/Loading.js
@@ -1,19 +1,54 @@
 // Importing react utilities
 import React from 'react';
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import LottieView from 'lottie-react-native';
 
-export default function Loading() {
-  return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#5b3a70" />
+export default class Loading extends React.Component {
+  componentDidMount() {
+    this.animation.play();
+    // Or set a specific startFrame and endFrame with:
+    // this.animation.play(30, 120);
+  }
+
+  resetAnimation = () => {
+    this.animation.reset();
+    this.animation.play();
+  };
+
+  render() {
+    return (
+      <View style={styles.animationContainer}>
+        {/* <Image source={require('../../assets/animations/bird.png')} resizeMode="contain" style={styles.logo}></Image> */}
+        <LottieView
+          ref={animation => {
+            this.animation = animation;
+          }}
+          style={{
+            width: 200,
+            height: 200,
+          }}
+          speed={0.7}
+          source={require('../../assets/animations/loading.json')}
+        />
       </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
+  animationContainer: {
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+  logo: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+    height: '50%',
   },
 });

02f3789f638e2631ed2b97dc59560a57c8f1fe0e
Santiago <santiperez1199@gmail.com>
2021-05-08 19:44:08 +0200

    Getting channel name

--- src/chatkitty/index.js
+++ src/chatkitty/index.js
@@ -1,3 +1,15 @@
 import ChatKitty from 'chatkitty';
 
 export const kitty = ChatKitty.getInstance('9bf331de-fc02-4f97-bbba-0a329051841a');
+
+export function getChannelDisplayName(channel, user) {
+    if (channel.type === 'DIRECT') {
+        if (channel.members[0].displayName == user.displayName) {
+            return channel.members[1].displayName;
+        }else{
+            return channel.members[0].displayName;
+        }
+    } else {
+        return channel.name;
+    }
+}
